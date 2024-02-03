85502f212c2fbe077bbeadfc5813c49ce6ca4112
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-03 15:55:14 +0530

    1. refactor: refactor main func using fetcher

--- projects/output-and-error-handling/client/main.go
+++ projects/output-and-error-handling/client/main.go
@@ -1,17 +1,33 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/chettriyuvraj/immersive-go-course/projects/output-and-error-handling/client/fetcher"
+)
+
+const URL = "http://localhost:8080"
+
 func main() {
-	// for {
-	// 	respStr, err := MakeRequest()
-	// 	if err != nil {
-	// 		if errors.Is(err, ClientUnexpectedError) {
-	// 			fmt.Fprintf(os.Stderr, "\nerror: irrevcoverable error: %v", err)
-	// 			os.Exit(1)
-	// 		}
-	// 		fmt.Fprintf(os.Stderr, "\nerror: %v", err)
-	// 		continue
-	// 	}
-	// 	fmt.Printf("\n%v", respStr)
-	// 	time.Sleep(3 * time.Second)
-	// }
+	f := fetcher.NewFetcher(http.DefaultTransport)
+	for {
+		respStr, err := f.MakeRequest(URL)
+		if err != nil {
+
+			switch {
+			case errors.As(err, &fetcher.ClientUnexpectedError):
+				fmt.Fprintf(os.Stderr, "\nerror: irrevcoverable error: %v", err)
+				os.Exit(1)
+			case errors.As(err, &fetcher.RetryError{}):
+				fmt.Fprintf(os.Stderr, "\nerror: retrying", err)
+				continue
+			}
+		}
+		fmt.Printf("\n%v", respStr)
+		time.Sleep(2 * time.Second)
+	}
 }

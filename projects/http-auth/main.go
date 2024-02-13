088cb892479deb3ce11021b6d05ec7c6bc4cdb2b
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-13 18:33:24 +0530

    1. feat: update server to handle 200, 404 and 500

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -3,8 +3,17 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world!"))
+
+	http.HandleFunc("/", http.NotFoundHandler().ServeHTTP)
+
+	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200"))
+	})
+
+	http.HandleFunc("/500", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
 	})
 
 	http.ListenAndServe(":8080", nil)

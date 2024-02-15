0ffdcfb07f2fcbd13509ccdfe97ef85dd4cc0725
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-15 18:25:48 +0530

    1. feat: modify '/' to allow get requests again 2. feat: change '/' handler to return query params as html + escape query params before returning

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"bytes"
-	"io"
+	"fmt"
+	"html"
 	"net/http"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" || r.Method != http.MethodPost {
+		if r.URL.Path != "/" {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("404 page not found"))
 			return
 		}
 
 		buf := new(bytes.Buffer)
-		defer r.Body.Close()
-		_, err := io.Copy(buf, r.Body) // not needed - but just to show that we can maybe do some processing stuff here
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		buf.Write([]byte("<em>Hello world</em>\n"))
+		buf.Write([]byte("<ul>\n"))
+		queryParams := r.URL.Query()
+		for k, vList := range queryParams {
+			v := vList[0]
+			v = html.EscapeString(v)
+			buf.Write([]byte(fmt.Sprintf("<li>%s: %s</li>\n", k, v)))
 		}
 
 		w.Header().Set("Content-Type", "text/html")
-		w.Write(buf.Bytes())
+		buf.Write([]byte("</ul>\n"))
+		_, err := w.Write(buf.Bytes())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
 	})
 
 	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {

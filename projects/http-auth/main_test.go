3e085c56ea77adf4e43ac4ecad9907653668bdf3
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-02-17 00:15:53 +0530

    1. feat: add test for "/authenticated" path

--- projects/http-auth/main_test.go
+++ projects/http-auth/main_test.go
@@ -109,3 +109,80 @@ func TestInvalidURLs(t *testing.T) {
 		})
 	}
 }
+
+func TestHandleAuthenticated(t *testing.T) {
+	url := fmt.Sprintf("%s/authenticated", DUMMYURL)
+	type args struct {
+		req     *http.Request
+		headers map[string]string
+	}
+	type resp struct {
+		respStr    string
+		statusCode int
+		headers    map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		resp resp
+	}{
+		{
+			name: "request without credentials",
+			args: args{
+				req: httptest.NewRequest(http.MethodGet, url, nil),
+			},
+			resp: resp{
+				statusCode: http.StatusUnauthorized,
+				headers: map[string]string{
+					"WWW-Authenticate": `Basic realm = "Access to the staging site"`,
+				},
+			},
+		},
+		{
+			name: "request with valid credentials",
+			args: args{
+				req: httptest.NewRequest(http.MethodGet, url, nil),
+				headers: map[string]string{
+					"Authorization": "Basic Y2hldHRyaXl1dnJhajpraW5n", //"chettriyuvraj:king"
+				},
+			},
+			resp: resp{
+				respStr:    "<!DOCTYPE html>\n<html>Hello chettriyuvraj!",
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "request with invalid credentials",
+			args: args{
+				req: httptest.NewRequest(http.MethodGet, url, nil),
+				headers: map[string]string{
+					"Authorization": "Basic G2hldHRyaXl1dnJhajpraW5n", //"chettriyuvraj:king"
+				},
+			},
+			resp: resp{
+				respStr:    "Credentials wrong or invalid",
+				statusCode: http.StatusUnauthorized,
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			for k, v := range tc.args.headers {
+				tc.args.req.Header.Set(k, v)
+			}
+			handleAuthenticated(w, tc.args.req)
+
+			resp := w.Result()
+			buf := new(bytes.Buffer)
+			_, err := io.Copy(buf, resp.Body)
+			require.NoError(t, err)
+			require.Equal(t, tc.resp.respStr, buf.String())
+			require.Equal(t, tc.resp.statusCode, resp.StatusCode)
+			require.Equal(t, len(tc.resp.headers), len(resp.Header))
+			for k := range tc.resp.headers {
+				require.Equal(t, tc.resp.headers[k], resp.Header.Get(k))
+			}
+		})
+	}
+}

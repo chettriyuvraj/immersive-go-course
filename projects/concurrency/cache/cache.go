a29816b52965e5f9a204726c9eaaa5f82948ab09
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-23 19:27:48 +0530

    1. refactor: change cache entry struct name to cacheNode

--- projects/concurrency/cache/cache.go
+++ projects/concurrency/cache/cache.go
@@ -9,17 +9,17 @@ type CacheStats struct {
 	totalReads, totalWrites int
 }
 
-type CacheEntry[V any] struct {
+type CacheNode[V any] struct {
 	val   V
 	reads int
 }
 
-func (ce *CacheEntry[V]) isUntouched() bool {
+func (ce *CacheNode[V]) isUntouched() bool {
 	return ce.reads == 0
 }
 
 type Cache[K comparable, V any] struct {
-	cacheMap             map[K]CacheEntry[V]
+	cacheMap             map[K]CacheNode[V]
 	size                 int
 	mu                   sync.Mutex
 	hits, misses, writes int
@@ -29,7 +29,7 @@ type Cache[K comparable, V any] struct {
 /* Always initialize new cache using this function */
 func NewCache[K comparable, V any](size int) *Cache[K, V] {
 	return &Cache[K, V]{
-		cacheMap: make(map[K]CacheEntry[V]),
+		cacheMap: make(map[K]CacheNode[V]),
 		size:     size,
 	}
 }
@@ -38,22 +38,22 @@ func (c *Cache[K, V]) Get(key K) (V, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	cachEntry, exists := c.cacheMap[key]
+	node, exists := c.cacheMap[key]
 	if !exists {
 		c.misses++
 		return getZero[V](), false
 	}
 
 	c.hits++
-	cachEntry.reads++
-	return cachEntry.val, true
+	node.reads++
+	return node.val, true
 }
 
 func (c *Cache[K, V]) Put(key K, val V) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.cacheMap[key] = CacheEntry[V]{val: val}
+	c.cacheMap[key] = CacheNode[V]{val: val}
 	c.size++
 	c.writes++
 }

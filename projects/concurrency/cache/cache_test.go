4fe21add834a7656b7073a5c90587e2a64327d98
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-23 13:21:40 +0530

    feat: add test for cache stats

--- projects/concurrency/cache/cache_test.go
+++ projects/concurrency/cache/cache_test.go
@@ -11,9 +11,9 @@ func TestCachePutAndGet(t *testing.T) {
 	cache := NewCache[string, string](5)
 	key, val := "testKey", "testVal"
 	cache.Put(key, val)
-	cachedVal, isCached := cache.Get(key) /* Note that Get returns a pointer */
+	cachedVal, isCached := cache.Get(key)
 	require.Equal(t, true, isCached)
-	require.Equal(t, val, *cachedVal)
+	require.Equal(t, val, cachedVal)
 }
 
 func TestCachePutAndGetParallel(t *testing.T) {
@@ -31,6 +31,47 @@ func TestCachePutAndGetParallel(t *testing.T) {
 		for i := 0; i < 100; i++ {
 			cache.Put("k1", "v1")
 		}
+	})
+}
+
+func TestCacheStatsUpdate(t *testing.T) {
+	cache := NewCache[string, string](500)
+	t.Run("Test cache hit count", func(t *testing.T) {
+		t.Parallel()
+		iterations := 50
+		key, val := "k1", "v1"
+		cache.Put(key, val)
+		for i := 0; i < iterations; i++ {
+			cachedVal, exists := cache.Get(key)
+			require.Equal(t, true, exists)
+			require.Equal(t, val, cachedVal)
+			require.Equal(t, i+1, cache.hits)
+		}
+
+	})
+
+	t.Run("Test cache miss count", func(t *testing.T) {
+		t.Parallel()
+		iterations := 50
+		key := "k2"
+		cache := NewCache[string, string](5)
+		for i := 0; i < iterations; i++ {
+			_, exists := cache.Get(key)
+			require.Equal(t, false, exists)
+			require.Equal(t, i+1, cache.misses)
+		}
+	})
+
+	t.Run("Test cache write count", func(t *testing.T) {
+		t.Parallel()
+		iterations := 50
+		key, val := "k3", "v3"
+		cache := NewCache[string, string](5)
+		for i := 0; i < iterations; i++ {
+			cache.Put(key, val)
+			require.Equal(t, i+1, cache.writes)
+		}
+
 	})
 
 }

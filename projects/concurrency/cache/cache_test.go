7d609cbce5d1e1242e5e5e829d39d1675abf4e6a
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-24 15:12:01 +0530

    1. test: clean up tests using ran function

--- projects/concurrency/cache/cache_test.go
+++ projects/concurrency/cache/cache_test.go
@@ -1,45 +1,52 @@
 package cache
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 /* TODO: How would you dynamically test caches of different types? */
-func TestCachePutAndGet(t *testing.T) {
-	cache := NewCache[string, string](5)
-	key, val := "testKey", "testVal"
+func TestPutAndGet(t *testing.T) {
+	cacheSize := 5
+	cache := NewCache[int, int](cacheSize)
+	rand := getRandGenerator()
+
+	key, val := rand.Int(), rand.Int()
 	cache.Put(key, val)
 	cachedVal, isCached := cache.Get(key)
 	require.Equal(t, true, isCached)
 	require.Equal(t, val, cachedVal)
 }
 
-func TestCachePutAndGetParallel(t *testing.T) {
-	cache := NewCache[string, string](5)
+func TestPutAndGetLock(t *testing.T) {
+	cacheSize := 5
+	cache := NewCache[int, int](cacheSize)
+	rand := getRandGenerator()
 
 	t.Run("Get cache in parallel", func(t *testing.T) {
 		t.Parallel()
 		for i := 0; i < 100; i++ {
-			cache.Get("k1")
+			cache.Get(rand.Int())
 		}
 	})
 
 	t.Run("Put cache in parallel", func(t *testing.T) {
 		t.Parallel()
 		for i := 0; i < 100; i++ {
-			cache.Put("k1", "v1")
+			cache.Put(rand.Int(), rand.Int())
 		}
 	})
 }
 
-func TestCacheStatsUpdate(t *testing.T) {
-	cache := NewCache[string, string](500)
+func TestStatsUpdate(t *testing.T) {
+	cacheSize, iterations := 500, 50
+	rand := getRandGenerator()
 	t.Run("Test cache hit count", func(t *testing.T) {
 		t.Parallel()
-		iterations := 50
-		key, val := "k1", "v1"
+		cache := NewCache[int, int](cacheSize)
+		key, val := rand.Int(), rand.Int()
 		cache.Put(key, val)
 		for i := 0; i < iterations; i++ {
 			cachedVal, exists := cache.Get(key)
@@ -47,14 +54,12 @@ func TestCacheStatsUpdate(t *testing.T) {
 			require.Equal(t, val, cachedVal)
 			require.Equal(t, i+1, cache.hits)
 		}
-
 	})
 
 	t.Run("Test cache miss count", func(t *testing.T) {
 		t.Parallel()
-		iterations := 50
-		key := "k2"
-		cache := NewCache[string, string](5)
+		cache := NewCache[int, int](cacheSize)
+		key := rand.Int()
 		for i := 0; i < iterations; i++ {
 			_, exists := cache.Get(key)
 			require.Equal(t, false, exists)
@@ -64,14 +69,27 @@ func TestCacheStatsUpdate(t *testing.T) {
 
 	t.Run("Test cache write count", func(t *testing.T) {
 		t.Parallel()
-		iterations := 50
-		key, val := "k3", "v3"
-		cache := NewCache[string, string](5)
+		cache := NewCache[int, int](cacheSize)
+		key, val := rand.Int(), rand.Int()
 		for i := 0; i < iterations; i++ {
 			cache.Put(key, val)
 			require.Equal(t, i+1, cache.writes)
 		}
-
 	})
+}
 
+// func TestLRU(t *Testing.T) {
+// 	cacheSize := 5
+
+// 	t.Run("Fill cache till limit") {
+
+// 	}
+
+// }
+
+/* Pseudo-random number with same seed */
+func getRandGenerator() *rand.Rand {
+	seed := 500
+	src := rand.NewSource(int64(seed))
+	return rand.New(src)
 }
